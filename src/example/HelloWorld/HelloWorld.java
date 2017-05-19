e93207b73f48d15d894cf196afab625af26a8da3
Avinash kumar <avigr8_hak@hotmail.com>
2017-05-19 07:33:05 +0530

    Add files via upload

---
+++ src/example/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package example.HelloWorld;
+
+// ACDGILD - Java B - Session 1 - Assignment 1
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("HelloWorld!");
+
+	}
+
+}
